ded12e821144b5418b81bf11c4a20f1df1e38cbc
Sangeeta Joseph <>
2020-10-05 22:44:18 +0200

    Updates related to the navigation in the app

--- react/supply-chain-ui/src/App.js
+++ react/supply-chain-ui/src/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import RegisterUser from './components/RegisterUser';
 import CreateOrder from './components/create-order/CreateOrder';
-import { BrowserRouter, Route, Link, Router, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import ProductHistory from './components/product-history/ProductHistory';
+import AccessRequestList from './components/access-request/AccessRequestList'
+import AccessRequestDetails from './components/access-request/AccessRequestDetails'
+import AccessRequestToApproveList from './components/access-request-to-approve/AccessRequestToApproveList';
 
 function App() {
   return (
     <div className="App">
 
-      <BrowserRouter>
-        <Link exact to='/createOrder'>Click</Link>
-        <Link exact to='/productHistory'>Click 2</Link>
+      <Switch>
         <Route exact path="/" component={RegisterUser}></Route>
-        <Route exact path="/createOrder" component={CreateOrder}></Route>
-        <Route exact path="/productHistory" component={ProductHistory}></Route>
+        <Route path="/createOrder" component={CreateOrder}></Route>
+        <Route path="/productHistory" component={ProductHistory}></Route>
+        <Route path="/accessRequestList" component={AccessRequestList}></Route>
+        <Route path="/accessRequestDetails" component={AccessRequestDetails}></Route>
+        <Route path="/accessRequestToApproveList" component={AccessRequestToApproveList}></Route>
+      </Switch>
+      <BrowserRouter>
+        <Link to='/createOrder'>Click</Link>
+        <Link to='/productHistory'>Click 2</Link>
+        <Link to='/accessRequestList'>Click 3</Link>
+        <Link to='/accessRequestToApproveList'>Click 4</Link>
+
       </BrowserRouter>
 
     </div>
